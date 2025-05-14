9fde7ec8add117b9a35cc2738a6d7f364d7fe9c5
tonyimax <lin_hong_quan_msn@hotmail.com>
2025-05-14 11:29:10 +0800

    tcp and udp server and client ok

--- Udp.cs
+++ Udp.cs
@@ -1,6 +1,81 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace net_conn;
 
 public class Udp
 {
+    private String _host = "127.0.0.1";
+    private int _port = 8850;
+
     
+    public string Host
+    {
+        get => _host;
+        set => _host = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    private int Port
+    {
+        get => _port;
+        set => _port = value;
+    }
+
+    public void Server()
+    {
+        UdpClient uc1 = new UdpClient(Port);
+        Byte[] buf = Encoding.Default.GetBytes("Hello Client");
+        
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        Byte[] rBuf = uc1.Receive(ref RemoteIpEndPoint);
+        string data = Encoding.Default.GetString(rBuf);
+        Console.WriteLine("<==={0}",data);
+
+        uc1.Send(buf, RemoteIpEndPoint);
+        Console.WriteLine("===>{0}",Encoding.Default.GetString(buf));
+    }
+    
+    public void Client()
+    {
+        UdpClient uc1 = new UdpClient(Port+1);
+        uc1.Connect(Host,Port);
+        Byte[] buf = Encoding.Default.GetBytes("Hello Server");
+        uc1.Send(buf, buf.Length);
+        Console.WriteLine("===>{0}",Encoding.Default.GetString(buf));
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        Byte[] rBuf = uc1.Receive(ref RemoteIpEndPoint);
+        string data = Encoding.Default.GetString(rBuf);
+        Console.WriteLine("<==={0}",data);
+    }
+
+    public void Send()
+    {
+        UdpClient uc1 = new UdpClient(Port);//for local udp
+        try{
+            uc1.Connect(Host, Port);//for remote udp
+            Byte[] buf = Encoding.Default.GetBytes("UDP DATA HELLO");
+            uc1.Send(buf, buf.Length);
+
+            //UdpClient udpClientB = new UdpClient();
+            //udpClientB.Send(buf, buf.Length, "WK_QT_DEV",Port);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Byte[] rBuf = uc1.Receive(ref RemoteIpEndPoint);
+            string data = Encoding.Default.GetString(rBuf);
+            
+            Console.WriteLine("This is the message you received " +
+                              data.ToString());
+            Console.WriteLine("This message was sent from " +
+                              RemoteIpEndPoint.Address.ToString() +
+                              " on their port number " +
+                              RemoteIpEndPoint.Port.ToString());
+
+            uc1.Close();
+            //uc2.Close();
+        }
+        catch (Exception e ) {
+            Console.WriteLine(e.ToString());
+        }
+    }
 }
